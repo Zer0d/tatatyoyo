0e1cfe96ee35d705f3786a78b1f708dd60fa0f20
zerod <zerod@dius.com.au>
2012-10-02 13:15:17 +1000

    Initial zerod configuration

---
+++ TESTWORKADRIAN/grails-app/conf/DataSource.groovy
@@ -0,0 +1,12 @@
+dataSource {
+    pooled = true
+}
+hibernate {
+    cache.use_second_level_cache = true
+    cache.use_query_cache = false
+    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+}
+// environment specific settings
+environments {
+    
+}
